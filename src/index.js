e0a0c378beb21ef855c2e608e797681c7644122e
Anton Paluykh <anton.paluykh@gmail.com>
2021-03-08 16:15:08 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let str = String(n);
+    let arr = str.split('');
+    let myNum = +(arr.reverse().join(''));
+    return myNum;
 }
